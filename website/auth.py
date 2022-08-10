176b4385eeeac824b5b10e4d79d934e342302ecf
FailedRift <ikennaaokon@gmail.com>
2022-08-10 12:26:09 +0100

    A Note taking application. Lets you signup sign in and keeps, add notes, keeps notes. Using Flask. Html, CSS bootstrap and SQL database.

---
+++ website/auth.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template, request
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    data = request.form
+    print(data)
+    return render_template("login.html")
+
+@auth.route("/logout")
+def logout():
+    return render_template("home.html")
+
+@auth.route('/sign_up', methods=['GET', 'POST'])
+def signup():
+    return render_template("sign_up.html")
+
+
